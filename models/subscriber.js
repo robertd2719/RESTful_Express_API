6b6c22550b8992b777a602bdd312d4e534a0215d
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2020-03-12 21:19:23 -0400

    added our schema file to our model

---
+++ models/subscriber.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const subscriberSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true;
+    },
+    subscribedToChannel:{
+        type: String,
+        required: true;
+    },
+    subscribeDate:{
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+// the mongoose.model takes in two parameters the Model in the database
+// we are working with and its schema
+modules.exports = mongoose.model('Subscriber',subscriberSchema)
