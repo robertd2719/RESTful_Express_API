7a3b6237648f64cc8f4e2e1134dacef51cdbf946
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2020-03-12 21:09:58 -0400

    small update to last some files were not included

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -11,7 +11,20 @@ router.get('/',(req, res) => {
 router.get('/:id',(req, res) => {
     res.status(200).json({ value: req.params.id});
 })
-// creating one
-// updating one
+// creating one --this is a post request as we are adding something to our db/collection
+router.post('/',(req, res) => {
+    
+})
+
+// updating one, we are using path instead of put as we are only going to be updating based
+// solely on what the user is actually passing to us. PUT would update all of the information
+// to the subscriber at once and this is behavior that is not intended.
+router.patch('/:id',(req, res) => {
+    
+})
+
 // deleting one
+router.delete('/:id',(req,res) => {
+    
+})
 module.exports = router
